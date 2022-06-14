b8f4f69f461e98250ac4027cc1cc93341f78ce9a
Plus1XP <evldev@protonmail.ch>
2022-06-14 15:43:23 +0100

    Feat: Add dispatch que

--- ObservableObject/ContactViewModel.swift
+++ ObservableObject/ContactViewModel.swift
@@ -16,17 +16,17 @@ class ContactsViewModel: ObservableObject {
     func requestAccess() {
         switch CNContactStore.authorizationStatus(for: .contacts) {
         case .authorized:
-            self.getContacts()
+            self.fetchContacts()
         case .denied:
             contactStore.requestAccess(for: .contacts) { granted, error in
                 if granted {
-                    self.getContacts()
+                    self.fetchContacts()
                 }
             }
         case .restricted, .notDetermined:
             contactStore.requestAccess(for: .contacts) { granted, error in
                 if granted {
-                    self.getContacts()
+                    self.fetchContacts()
                 }
             }
         @unknown default:
@@ -36,33 +36,24 @@ class ContactsViewModel: ObservableObject {
 
     func fetchContacts() -> Void {
         contacts.removeAll()
-        contactStore.requestAccess(for: .contacts) { (granted, error) in
-            if let error = error {
-                print("failed to request access", error)
-                return
-            }
-            if granted {
-                let key = [CNContactGivenNameKey,CNContactFamilyNameKey,CNContactPhoneNumbersKey,CNContactEmailAddressesKey, CNContactThumbnailImageDataKey] as [CNKeyDescriptor]
-                let request = CNContactFetchRequest(keysToFetch: key)
-                request.sortOrder = CNContactSortOrder.userDefault
-                
-                try? self.contactStore.enumerateContacts(with: request, usingBlock: { (contact, stoppingPointer) in
-                    let givenName = contact.givenName
-                    let familyName = contact.familyName
-                    let emailAddress = contact.emailAddresses.first?.value ?? ""
-                    let phoneNumber: [String] = contact.phoneNumbers.map{ $0.value.stringValue }
-                    let identifier = contact.identifier
-                    var image = UIImage()
-                    if contact.thumbnailImageData != nil {
-                        image = UIImage(data: contact.thumbnailImageData!)!
-                    }
-                    self.contacts.append(ContactsModel(givenName: givenName, familyName: familyName, phoneNumber: phoneNumber, emailAddress: emailAddress as String, identifier: identifier, image: image))
-                    debugPrint("\(givenName) \(familyName) \(identifier)")
-                })
-                self.sectionDictionary = self.getSectionedDictionary()
-            } else {
-                print("access denied")
-            }
+        let key = [CNContactGivenNameKey,CNContactFamilyNameKey,CNContactPhoneNumbersKey,CNContactEmailAddressesKey, CNContactThumbnailImageDataKey] as [CNKeyDescriptor]
+        let request = CNContactFetchRequest(keysToFetch: key)
+        request.sortOrder = CNContactSortOrder.userDefault
+        DispatchQueue.main.async {
+            try? self.contactStore.enumerateContacts(with: request, usingBlock: { (contact, stoppingPointer) in
+                let givenName = contact.givenName
+                let familyName = contact.familyName
+                let emailAddress = contact.emailAddresses.first?.value ?? ""
+                let phoneNumber: [String] = contact.phoneNumbers.map{ $0.value.stringValue }
+                let identifier = contact.identifier
+                var image = UIImage()
+                if contact.thumbnailImageData != nil {
+                    image = UIImage(data: contact.thumbnailImageData!)!
+                }
+                self.contacts.append(ContactsModel(givenName: givenName, familyName: familyName, phoneNumber: phoneNumber, emailAddress: emailAddress as String, identifier: identifier, image: image))
+                debugPrint("\(givenName) \(familyName) \(identifier)")
+            })
+            self.sectionDictionary = self.getSectionedDictionary()
         }
     }
     
@@ -75,15 +66,17 @@ class ContactsViewModel: ObservableObject {
                 print("No contacts found")
                 return
             }
-            for contact in contacts {
-                let mutableContact = contact.mutableCopy() as! CNMutableContact
-                let req = CNSaveRequest()
-                req.delete(mutableContact)
-                do {
-                    try self.contactStore.execute(req)
-                    print("Success, You deleted: \(contact.givenName) \(contact.familyName) \(contact.identifier)")
-                } catch let err {
-                    print("Error = \(err)")
+            DispatchQueue.main.async {
+                for contact in contacts {
+                    let mutableContact = contact.mutableCopy() as! CNMutableContact
+                    let req = CNSaveRequest()
+                    req.delete(mutableContact)
+                    do {
+                        try self.contactStore.execute(req)
+                        print("Success, You deleted: \(contact.givenName) \(contact.familyName) \(contact.identifier)")
+                    } catch let err {
+                        print("Error = \(err)")
+                    }
                 }
             }
         } catch let fetchError {
